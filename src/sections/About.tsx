27c62cb4179428a2d9a495ddd3d5f7e3d9bc3e0d
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-20 18:19:09 -0300

    inicia a section about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,35 @@
+import { Code2, Lightbulb, Rocket, Users } from "lucide-react"
+
+const destaque = [
+    {
+        icon: Code2,
+        titulo: "Clean Code",
+        descricao: "faucibus augue. Fusce fringilla neque metus, quis viverra dolor egestas ut."
+    },
+    {
+        icon: Rocket,
+        titulo: "Performance",
+        descricao: "faucibus augue. Fusce fringilla neque metus, quis viverra dolor egestas ut."
+    },
+    {
+        icon: Users,
+        titulo: "Collaboration",
+        descricao: "faucibus augue. Fusce fringilla neque metus, quis viverra dolor egestas ut."
+    },
+    {
+        icon: Lightbulb,
+        titulo: "Innovation",
+        descricao: "faucibus augue. Fusce fringilla neque metus, quis viverra dolor egestas ut."
+    },
+]
+
 function About(){
     return <section>
-   
+        <div>
+            <div>
+                
+            </div>
+        </div>
     </section>
 }
 
