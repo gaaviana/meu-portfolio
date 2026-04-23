2b60c500e626dc57284d0f988b0676ccec5aa255
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-23 02:10:56 -0300

    add props react.button
    para o button herdar todas as função do button do html

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,34 @@
-type ButtonProps = {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string
   size?: "sm" | "default" | "lg"
   children: React.ReactNode
 }
 
-function Button({className = "", size = "default", children}: ButtonProps) {
-    const baseClasses = "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-primaria bg-primaria text-primaria-texto hover:bg-primaria/90 shadow-lg shadow-primaria/25";
+function Button({
+  className = "",
+  size = "default",
+  children,
+  ...props
+}: ButtonProps) {
 
-    const sizeClasses = {
-        sm: "px-4 py-2 text-sm",
-        default: "px-6 py-3 text-base",
-        lg: "px-8 py-4 text-lg",
-    }
+  const baseClasses =
+    "relative overflow-hidden rounded-full font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-primaria bg-primaria text-primaria-texto hover:bg-primaria/90 shadow-lg shadow-primaria/25";
 
-    const classes = `${baseClasses} ${sizeClasses[size]} ${className}`;
-    return <button className={classes}>
-        <span className="relative flex items-center justify-center gap-2">{children}</span>
+  const sizeClasses = {
+    sm: "px-4 py-2 text-sm",
+    default: "px-6 py-3 text-base",
+    lg: "px-8 py-4 text-lg",
+  }
+
+  const classes = `${baseClasses} ${sizeClasses[size]} ${className}`;
+
+  return (
+    <button className={classes} {...props}>
+      <span className="relative flex items-center justify-center gap-2">
+        {children}
+      </span>
     </button>
+  )
 }
 
 export default Button
