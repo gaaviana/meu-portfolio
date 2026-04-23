9e530e5698dd3c6b87f7b6022f7dd3ff9db45379
Gabriel Viana <gaa.vianaa@gmail.com>
2026-04-23 02:10:22 -0300

    cria component AnimatedButton

--- src/components/AnimatedButton.tsx
+++ src/components/AnimatedButton.tsx
@@ -1,7 +1,9 @@
-import { Download } from "lucide-react"
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode
+}
 
-function AnimatedButton() {
-    return <button className="relative bg-transparent border border-border text-texto hover:border-primaria/50 transition-all duration-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-primaria focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed group px-8 py-4 text-lg font-medium rounded-full overflow-visible animacao-borda">  <span className="relative z-10 flex items-center justify-center gap-2"><Download className="w-5 h-5" /> Download CV </span></button>
+function AnimatedButton({children}:ButtonProps) {
+    return <button className="relative bg-transparent border border-border text-texto hover:border-primaria/50 transition-all duration-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-primaria focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed group px-8 py-4 text-lg font-medium rounded-full overflow-visible animacao-borda">  <span className="relative z-10 flex items-center justify-center gap-2">{children}</span></button>
 }
 
 export default AnimatedButton
